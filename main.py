52810af7ce916c8f353e10808e6dcbf11bff6596
Norihisa Yanai <yanaitti@gmail.com>
2021-02-11 15:37:34 +0900

    localize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response, render_template, url_for
+from flask import Flask, Response, render_template, url_for, request
+from flask_babel import Babel, gettext
 from flask_caching import Cache
 import uuid
 import random
@@ -9,6 +10,12 @@ import copy
 import numpy as np
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['ja', 'ja_JP', 'en'])
 
 
 @app.context_processor
